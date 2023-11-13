6e3d4924e15918c7aa3b30f6532d6128d3f94e65
Minjae Lee <113198237+justinminlee@users.noreply.github.com>
2023-11-14 00:45:41 +1000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,128 +1,63 @@
+// src/App.js
 import React, { useState } from 'react';
-import ConvertApi from 'convertapi-js';
+import axios from 'axios';
 
-// state management
-const FileCompression = () => {
-  const [file, setFile] = useState(null);
-  const [inputFormat, setInputFormat] = useState('docx');
-  const [outputFormat, setOutputFormat] = useState('pdf');
-  const [result, setResult] = useState('');
+function App() {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [compressionFormat, setCompressionFormat] = useState('zip');
+  const [downloadLink, setDownloadLink] = useState(null);
 
-  // handles file input change event
   const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
+    const files = e.target.files;
+    setSelectedFiles([...selectedFiles, ...files]);
   };
 
-  // file compression process
-  const compressFile = async () => {
+  const handleCompressionFormatChange = (e) => {
+    setCompressionFormat(e.target.value);
+  };
+
+  const handleCompression = async () => {
+    if (selectedFiles.length === 0) {
+      console.error('No files selected for compression');
+      return;
+    }
+
+    const formData = new FormData();
+    selectedFiles.forEach((file) => {
+      formData.append('files', file);
+    });
+
     try {
-      if (!file) {
-        setResult('No file selected for compression');
-        return;
-      }
-
-      const formData = new FormData();
-      formData.append('file', file);
-
-      const response = await fetch('/compress', {
-        method: 'POST',
-        body: formData,
+      const response = await axios.post(`http://localhost:3000/compress?format=${compressionFormat}`, formData, {
+        responseType: 'blob',
       });
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
+      const downloadUrl = (window.URL || window.webkitURL).createObjectURL(new Blob([response.data]));
 
-      const data = await response.json();
-      setResult(`Compressed file available at: ${data.compressedFile}`);
+      setDownloadLink(downloadUrl);
+
+      // Clean up
+      (window.URL || window.webkitURL).revokeObjectURL(downloadUrl);
     } catch (error) {
-      setResult(`Error: ${error.message}`);
-    }
-  };
-  //  file conversion process.
-  const convertFile = async () => {
-    try {
-      if (!file) {
-        setResult('No file selected for conversion');
-        return;
-      }
-
-      // https://www.convertapi.com/
-
-      let convertApi = ConvertApi.auth('LjMHwWnECuHQK6ZV');
-      let params = convertApi.createParams();
-
-      if (params.addFile) {
-        params.addFile(file, 'inputFile');
-      } else {
-        params = {
-          inputFile: file,
-        };
-      }
-
-      let conversionResult;
-      if (inputFormat === 'docx' && outputFormat === 'pdf') {
-        conversionResult = await convertApi.convert('docx', 'pdf', params);
-      } else if (inputFormat === 'xlsx' && outputFormat === 'pdf') {
-        conversionResult = await convertApi.convert('xlsx', 'pdf', params);
-      } else if (inputFormat === 'pptx' && outputFormat === 'pdf') {
-        conversionResult = await convertApi.convert('pptx', 'pdf', params);
-      } else if (inputFormat === 'docx' && outputFormat === 'html') {
-        conversionResult = await convertApi.convert('docx', 'html', params);
-      } else if (inputFormat === 'xls' && outputFormat === 'csv') {
-        conversionResult = await convertApi.convert('xls', 'csv', params);
-      } else if (inputFormat === 'docx' && outputFormat === 'compare') {
-        conversionResult = await convertApi.convert('docx', 'compare', params);
-      } else {
-        throw new Error('Unsupported conversion scenario');
-      }
-
-      setResult(`Conversion successful. Result URL: ${conversionResult.response.Files[0].Url}`);
-    } catch (error) {
-      setResult(`Error: ${error.message}`);
+      console.error('Error during compression:', error);
     }
   };
 
   return (
-    <div>
-      <h1>File Compression</h1>
-      <p>Select a file to compress:</p>
-      <input type="file" onChange={handleFileChange} accept=".txt, .pdf, .jpg, .png, .zip" required />
-      <button type="button" onClick={compressFile}>
-        Compress File
-      </button>
-
-      <div>{result}</div>
-
-      <h1>File Conversion</h1>
-      <p>Select a file to convert:</p>
-
-      <input type="file" onChange={handleFileChange} required />
-
-      <div>
-        <label htmlFor="inputFormat">Input Format:</label>
-        <select id="inputFormat" value={inputFormat} onChange={(e) => setInputFormat(e.target.value)}>
-          <option value="docx">DOCX</option>
-          <option value="xlsx">XLSX</option>
-          {/* Add more input format options as needed */}
+    <div className="App">
+      <h1>File Compression App</h1>
+      <input type="file" multiple onChange={handleFileChange} />
+      <label>
+        Compression Format:
+        <select value={compressionFormat} onChange={handleCompressionFormatChange}>
+          <option value="zip">ZIP</option>
+          <option value="tar.gz">Tar Gzip</option>
         </select>
-
-        <label htmlFor="outputFormat">Output Format:</label>
-        <select id="outputFormat" value={outputFormat} onChange={(e) => setOutputFormat(e.target.value)}>
-          <option value="pdf">PDF</option>
-          <option value="html">HTML</option>
-          <option value="csv">CSV</option>
-          {/* Add more output format options as needed */}
-        </select>
-
-        <button type="button" onClick={convertFile}>
-          Convert File
-        </button>
-      </div>
-
-      <div>{result}</div>
+      </label>
+      <button onClick={handleCompression}>Compress</button>
+      {downloadLink && <a href={downloadLink} download={`compressed.${compressionFormat}.bz2`}>Download Compressed File</a>}
     </div>
   );
-};
+}
 
-export default FileCompression;
+export default App;
